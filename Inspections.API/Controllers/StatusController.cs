95744d0d12c92f0c1fecfe33bf7fd411677a9d6a
bplus0 <boneill1292@gmail.com>
2022-02-17 20:11:47 -0600

    cleanup. CRUD is working

--- Inspections.API/Controllers/StatusController.cs
+++ Inspections.API/Controllers/StatusController.cs
@@ -1,109 +1,104 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Inspections.API.Data;
 using Inspections.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Inspections.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StatusController : ControllerBase
-    {
-        private readonly DataContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StatusController : ControllerBase
+	{
+		private readonly DataContext _context;
 
-        public StatusController(DataContext context)
-        {
-            _context = context;
-        }
+		public StatusController(DataContext context)
+		{
+			_context = context;
+		}
 
-        // GET: api/Status
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Status>>> GetStatuses()
-        {
-            return await _context.Statuses.ToListAsync();
-        }
+		// GET: api/Status
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Status>>> GetStatuses()
+		{
+			return await _context.Statuses.ToListAsync();
+		}
 
-        // GET: api/Status/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Status>> GetStatus(int id)
-        {
-            var status = await _context.Statuses.FindAsync(id);
+		// GET: api/Status/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Status>> GetStatus(int id)
+		{
+			var status = await _context.Statuses.FindAsync(id);
 
-            if (status == null)
-            {
-                return NotFound();
-            }
+			if (status == null)
+			{
+				return NotFound();
+			}
 
-            return status;
-        }
+			return status;
+		}
 
-        // PUT: api/Status/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutStatus(int id, Status status)
-        {
-            if (id != status.Id)
-            {
-                return BadRequest();
-            }
+		// PUT: api/Status/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutStatus(int id, Status status)
+		{
+			if (id != status.Id)
+			{
+				return BadRequest();
+			}
 
-            _context.Entry(status).State = EntityState.Modified;
+			_context.Entry(status).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StatusExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!StatusExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-            return NoContent();
-        }
+			return NoContent();
+		}
 
-        // POST: api/Status
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Status>> PostStatus(Status status)
-        {
-            _context.Statuses.Add(status);
-            await _context.SaveChangesAsync();
+		// POST: api/Status
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Status>> PostStatus(Status status)
+		{
+			_context.Statuses.Add(status);
+			await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetStatus", new { id = status.Id }, status);
-        }
+			return CreatedAtAction("GetStatus", new { id = status.Id }, status);
+		}
 
-        // DELETE: api/Status/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteStatus(int id)
-        {
-            var status = await _context.Statuses.FindAsync(id);
-            if (status == null)
-            {
-                return NotFound();
-            }
+		// DELETE: api/Status/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteStatus(int id)
+		{
+			var status = await _context.Statuses.FindAsync(id);
+			if (status == null)
+			{
+				return NotFound();
+			}
 
-            _context.Statuses.Remove(status);
-            await _context.SaveChangesAsync();
+			_context.Statuses.Remove(status);
+			await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+			return NoContent();
+		}
 
-        private bool StatusExists(int id)
-        {
-            return _context.Statuses.Any(e => e.Id == id);
-        }
-    }
+		private bool StatusExists(int id)
+		{
+			return _context.Statuses.Any(e => e.Id == id);
+		}
+	}
 }
